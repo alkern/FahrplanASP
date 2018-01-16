7db826a7b6a0c9b7e8a3fa1219bd2b396c26942f
Lukas Grätz <lukas@laptop.lan>
2018-01-16 21:41:12 +0100

    Better Output

---
+++ einfache_ausgabe.pl
@@ -0,0 +1,2 @@
+#show abfahrt_starthalt/3.
+#show fahrzeuge_benotigt/3.
---
+++ informative_ausgabe.pl
@@ -0,0 +1,7 @@
+#show anab/6.
+#show periode/2.
+#show fahrzeugminuten_benotigt/3.
+#show fahrzeuge_benotigt/3.
+#show fahrzeugtyp/2.
+
+
